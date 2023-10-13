98126175b95406e123e9997ffc33fea4c4df3ed6
Eneuem <nemozor@gmail.com>
2023-10-13 12:10:22 +0200

    00 + 01

---
+++ calculate.js
@@ -0,0 +1,8 @@
+function inputAndAdd() {
+    let x = parseFloat(prompt("Entrez une valeur pour x:"));
+    let y = parseFloat(prompt("Entrez une valeur pour y:"));
+    let z = x + y;
+    console.log("La somme de x et y est: " + z);
+}
+
+inputAndAdd();
